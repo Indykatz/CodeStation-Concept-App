eacc908c57e3d6a2d74e9a7e79d79d8c509a5b17
John Bridges <jbridges373@gmail.com>
2022-07-26 22:48:33 +0100

    add twitter icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -128,7 +128,7 @@ const Navbar = () => {
               className='flex justify-between items-center w-full text-gray-300'
               href='/'
             >
-              Email <FaTwitter size={30} />
+              Twitter <FaTwitter size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0376f9]'>
